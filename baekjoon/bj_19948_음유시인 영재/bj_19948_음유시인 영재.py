17aeba7d5f89982864c79b5a0dc067f087239a54
eunyoung <eunyoung950210@gmail.com>
2022-07-06 23:14:20 +0900

    pass

--- baekjoon/bj_19948_음유시인 영재/bj_19948_음유시인 영재.py
+++ baekjoon/bj_19948_음유시인 영재/bj_19948_음유시인 영재.py
@@ -1,33 +1,51 @@
-def make_title(content, alphabet):
+def make_title(content, space, alphabet):
+    old = 32
+    i = 0
+    N = len(content)
     title = ''
-    old = -1
 
-    for letter in content:
-        print(letter, old, alphabet[letter])
-        if old == letter:
-            continue
+    while i < N:
+        ascii = content[i]
 
-        elif old != letter and alphabet[letter]:
-            title += chr(letter+65)
-            alphabet[letter] -= 1
-            old = letter
+        # 이전 기록과 다른 경우
+        if old != ascii:
 
-        else:
-            return -1
+            # 스페이스 였을 때,
+            if ascii == 32:
+
+                # 입력 횟수가 남지 않았으면,
+                if not space:
+                    return -1   # 함수 종료
+
+                # 남았다면
+                space -= 1      # 입력 가능 횟수 차감
+                old = ascii     # 이전 기록 갱신
+
+            # 문자였을 때,
+            else:
+
+                # 소문자인 경우 대문자 코드로 변환(해당 알파벳 입력 가능한지 확인하기 위해 통일)
+                if ascii >= 97:
+                    ascii -= 32
+
+                if not alphabet[ascii]:
+                    return -1
+
+                if old == 32:
+                    title += chr(ascii)
+                    content.append(ascii)
+                    N += 1
+
+                alphabet[ascii] -= 1
+                old = content[i]
+
+        i += 1
 
     return title
 
 
 content = list(map(lambda x: ord(x), input()))
-# title = list(map(lambda x: ord((x[0].upper())) - 65, input().split()))
 space = int(input())
-alphabet = list(map(int, input().split()))
+alphabet = [0] * 65 + list(map(int, input().split()))
 
-print(content, space)
-English = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-print(list(zip(English, alphabet)))
-
-if space + 1 < len(content):
-    print(-1)
-else:
-    print(make_title(content, alphabet))
+print(make_title(content, space, alphabet))
