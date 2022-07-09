774605e2ec49582e37f8a20056c43ca0698007d4
eunyoung <eunyoung950210@gmail.com>
2022-07-09 17:09:32 +0900

    주석 추가

--- baekjoon/bj_1935_후위 표기식2/bj_1935_후위 표기식2.py
+++ baekjoon/bj_1935_후위 표기식2/bj_1935_후위 표기식2.py
@@ -1,9 +1,16 @@
 def postfix(notation):
     stack = []
 
+    # 후위 표기식에서 하나씩 순회하면서
     for elem in notation:
+
+        # 피연산자일 경우,
+        # stack에 삽입
         if elem not in "+-/*":
             stack.append(alpha_to_num[ord(elem)-ord('A')])
+
+        # 연산자일 경우,
+        # stack에 최근에 담긴 피연산자 2개를 뽑아 연산 후 삽입
         else:
             num2, num1 = stack.pop(), stack.pop()
             stack.append(calculate(num1, num2, elem))
@@ -14,10 +21,13 @@ def postfix(notation):
 def calculate(num1, num2, operator):
     if operator == '+':
         return num1 + num2
+
     elif operator == '-':
         return num1 - num2
+
     elif operator == '*':
         return num1 * num2
+
     else:
         return num1 / num2
 
