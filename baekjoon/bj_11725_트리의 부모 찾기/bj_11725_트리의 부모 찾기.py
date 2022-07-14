18ce5c37e036e807ffa2b328a119cf340ab0a722
eunyoung <eunyoung950210@gmail.com>
2022-07-14 23:59:58 +0900

    g

--- baekjoon/bj_11725_트리의 부모 찾기/bj_11725_트리의 부모 찾기.py
+++ baekjoon/bj_11725_트리의 부모 찾기/bj_11725_트리의 부모 찾기.py
@@ -1,27 +1,33 @@
 def BFS(v):
-    q = [v]
+    q = [0] * N
     tree = [0] * (N+1)
+    front = -1
+    rear = 0
+    q[rear] = v
     tree[v] = -1
 
-    while q:
-        parent = q.pop(0)
-        print(graph, parent, graph[parent])
+    while rear != front:
+        front += 1
+        parent = q[front]
+
         for node in graph[parent]:
             if not tree[node]:
                 tree[node] = parent
+                rear += 1
+                q[rear] = node
 
     return tree
 
 
 N = int(input())
-graph = [[0]*(N+1) for _ in range(N+1)]
-
+graph = [[] for _ in range(N+1)]
 
 for _ in range(N-1):
     v1, v2 = map(int, input().split())
-    graph[v1] = v2
-    graph[v2] = v1
+    graph[v1].append(v2)
+    graph[v2].append(v1)
 
 tree = BFS(1)
-for child in range(2, N+1):
-    tree[child]
+
+for child in tree[2:]:
+    print(child)
