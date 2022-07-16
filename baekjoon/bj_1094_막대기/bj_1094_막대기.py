7ece08d58e8766fae8b6de236061b36ab139ec49
noggrie <eunyoung950210@gmail.com>
2022-07-16 23:14:55 +0900

    bj_1967_set_input

---
+++ baekjoon/bj_1094_막대기/bj_1094_막대기.py
@@ -0,0 +1,10 @@
+x = int(input())
+
+cnt = 1
+stick = [32, 32]
+
+while sum(stick) > x:
+
+
+
+bj_10797_10부제
--- baekjoon/bj_11725_트리의 부모 찾기/bj_11725_트리의 부모 찾기.py
+++ baekjoon/bj_11725_트리의 부모 찾기/bj_11725_트리의 부모 찾기.py
@@ -2,9 +2,11 @@ N = int(input())  # 노드의 갯수
 
 tree = [[] for _ in range(N + 1)]
 print(tree)
-for i in range(N):
+for i in range(N-1):
     v1, v2 = map(int, input().split())
     print(v1, v2)
     tree[v1].append(v2)
 
 print(tree)
+
+bj_1094_막대기
